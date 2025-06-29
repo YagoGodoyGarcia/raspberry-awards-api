4e12585763e840ea0a97d007f3373b50bb1ee424
YagoGodoyGarcia <yagogodoygarcia@outlook.com>
2025-06-29 20:19:54 -0300

    configurando swagger

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger-ui', app, document);
 
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
